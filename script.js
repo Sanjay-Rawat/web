889be8476d56a4807699f017c7729c6ddf0d52bb
Sanjay Rawat <sanjay.rawat@saffrontech.net>
2021-05-25 17:11:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const userDetail = {
+  email: "sanjay32rawat@gmail.com",
+  name: "sanjay rawat",
+  address: {
+    post: "unyura",
+    pin: 253232,
+    email: "sanjay32rawat@gmail.com",
+    name: {
+      email: "sanjay32rawat@gmail.com",
+      name: "sanjay rawat",
+    },
+  },
+};
+console.trace(userDetail);
