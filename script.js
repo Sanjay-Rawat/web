40512e9d6bf3165d62ce5d5cf9f091c83ce054f6
Sanjay Rawat <sanjay.rawat@saffrontech.net>
2021-05-29 07:18:00 +0530

    29 may changes

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-const userDetail = {
-  email: "sanjay32rawat@gmail.com",
-  name: "sanjay rawat",
-  address: {
-    post: "unyura",
-    pin: 253232,
-    email: "sanjay32rawat@gmail.com",
-    name: {
-      email: "sanjay32rawat@gmail.com",
-      name: "sanjay rawat",
-    },
-  },
-};
-console.trace(userDetail);
+
+// const result = sum(5,10);
+// console.log(result)
+
+
+// function sum(first,second){
+//     return first + second;
+// }
+
+
+
+
+// const sum = function(first, second) {
+//     return first + second;
+// }
+
+// const result = sum(5,10);
+// console.log(result)
+
+
+// const sum = (first,second)=>{
+//     return first + second
+// }
+
+
+// const result = sum(5,10);
+// console.log(result)
+
+
+push ,pop, shift , unshift , slice ,splice 
+
+
+
+
+// const initial_array = [1,2,3,4];
+// const after_copy= Array.from(initial_array);
+
+// initial_array.push("last value")
+// console.log(after_copy)
+// console.log(initial_array)
+
+
+const arr=[12,23,24];
+// arr.forEach(function showValue(value){
+//     console.log(value)
+// })
+
+const a=arr.join('')
+console.log(a)
+
+
+
