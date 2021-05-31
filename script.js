252626519b41a2cec30fa5291ff4ec41443a57fd
Sanjay Rawat <sanjay.rawat@saffrontech.net>
2021-05-31 16:03:50 +0530

    today's work

--- script.js
+++ script.js
@@ -1,52 +1,83 @@
+// let arr= [1,2,3,4,5,6,7,8];
 
-// const result = sum(5,10);
-// console.log(result)
+// cos
 
-
-// function sum(first,second){
-//     return first + second;
+// const result =arr.filter(isEven);
+// function isEven(number){
+//     if(number%2==0){
+//         return true;
+//     }else{
+//        return false
+//     }
 // }
 
+// const data= [10,20,30,40];
 
+// console.log(data.reverse())
 
-
-// const sum = function(first, second) {
-//     return first + second;
-// }
-
-// const result = sum(5,10);
+// const result = data.map(addLastName)
 // console.log(result)
 
-
-// const sum = (first,second)=>{
-//     return first + second
+// function addLastName(number){
+//     return number +11
 // }
 
+// [].
 
-// const result = sum(5,10);
-// console.log(result)
+// var name="sanjay"
+// console.log(`my name is ${name}`);
+
+// const arr1=[1,2,3];
+// const arr2=[4,5,6];
+// const arr3= arr1.concat(arr2)
+// console.log(arr3)
+
+// const arr1=[1,2,3];
+// const arr2=[4,5,6];
+// const arr3=[...arr1, ...arr2]
+// console.log(arr3)
+
+// const studentData= {
+//     "photo_urls" : ["....abc.png" , "....xyz.gif"],
+//     videos_urls :[],
+//     created_by:{
+//         "name":"abc",
+//     },
+//     create_at:362187362
+// }
+
+// console.log(studentData.address)
+// // console.log(`My name is ${studentData.firstName} ${studentData.lastName}`)
+
+// ["hghj","gfhjgj","ghfhjgkj"]
+// const post =
+
+// const post =["sanjay","63426743","fhj"]
 
 
-push ,pop, shift , unshift , slice ,splice 
+const posts = [
+  {
+    text: 'post 1',
+    is_deleted: true,
+  },
+  {
+    text: 'post 2',
+    is_deleted: false,
+  },
+  {
+    text: 'post 3',
+    is_deleted: false,
+  },
+];
 
+const result = posts.filter(fiterPost);
+console.log(result);
 
-
-
-// const initial_array = [1,2,3,4];
-// const after_copy= Array.from(initial_array);
-
-// initial_array.push("last value")
-// console.log(after_copy)
-// console.log(initial_array)
-
-
-const arr=[12,23,24];
-// arr.forEach(function showValue(value){
-//     console.log(value)
-// })
-
-const a=arr.join('')
-console.log(a)
-
-
-
+function fiterPost(post) {
+  if (post.is_deleted == true) {
+    return false;
+  } else {
+    return true;
+  }
+  // return post.is_deleted == true ? false : true;
+}
